0c0e347c88abfb892c8060f508efad3c92def0bb
David Alarcon <momachilles@gmail.com>
2020-12-14 10:16:11 +0100

    Initial Commit

---
+++ Sibiel/SibielApp.swift
@@ -0,0 +1,17 @@
+//
+//  SibielApp.swift
+//  Sibiel
+//
+//  Created by David Alarcon on 14/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SibielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
